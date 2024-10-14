1114e1de187dee688c803e7d1f5881f0bb744c49
Jetalir <Jetalir@hotmail.com>
2024-10-14 23:02:34 +0200

    Uppgiften klar

---
+++ Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    internal class ComputerPlayer : Player
+    {
+
+        public ComputerPlayer(string Userid) : base(Userid)
+        {
+            
+        }
+    }
+}
---
+++ Hemuppgift_Arv_Temp/Game/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public abstract class Player
+    {
+        public string Userid;
+
+        public Player(string userid)
+        {
+            Userid = userid;
+        } 
+        public string getUserid()
+        {
+            return Userid;
+        }
+        public int TakePins(Board board, int pins) // Från board den ska ta bort antal pins från totalen
+        {
+            return board.TakePins(pins);
+        }
+
+
+    }
+}
